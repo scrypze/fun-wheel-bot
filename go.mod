58a8437605df8a64412adb0aed07c6c7cdecd413
eordiyants <erikray@mail.com>
2024-12-08 15:54:56 +0300

    sdsdf

--- go.mod
+++ go.mod
@@ -4,14 +4,20 @@ go 1.22
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/improbable-eng/grpc-web v0.15.0
 	google.golang.org/grpc v1.67.0
 	google.golang.org/protobuf v1.35.2
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
+	nhooyr.io/websocket v1.8.6 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -4,96 +4,95 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net"
+	"net/http"
 	"math/rand"
 	"time"
 
 	"google.golang.org/grpc"
-	pb "fun-wheel-bot/grpc" 
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	pb "fun-wheel-bot/grpc"
 )
 
-const port = ":50051"
-
 type server struct {
-    pb.UnimplementedFunWheelServiceServer
-    wheels map[int64][]string
-    rnd    *rand.Rand
+	pb.UnimplementedFunWheelServiceServer
+	wheels map[int64][]string
 }
 
 func (s *server) CreateWheel(ctx context.Context, req *pb.CreateWheelRequest) (*pb.CreateWheelResponse, error) {
-    log.Printf("Received CreateWheel request with chat_id: %d", req.GetChatId())
-    if s.wheels == nil {
-        log.Printf("Initializing wheels map")
-        s.wheels = make(map[int64][]string)
-    }
-    s.wheels[req.GetChatId()] = make([]string, 0)
-    log.Printf("Wheel created for chat_id: %d", req.GetChatId())
-    return &pb.CreateWheelResponse{Message: "Колесо создано!"}, nil
+	log.Printf("Creating wheel for chat_id: %d", req.GetChatId())
+	s.wheels[req.GetChatId()] = make([]string, 0)
+	return &pb.CreateWheelResponse{Message: "Колесо создано!"}, nil
 }
 
 func (s *server) AddOption(ctx context.Context, req *pb.AddOptionRequest) (*pb.AddOptionsResponse, error) {
-    log.Printf("Adding option '%s' for chat_id: %d", req.GetOption(), req.GetChatId())
-    
-    if _, exists := s.wheels[req.GetChatId()]; !exists {
-        return nil, fmt.Errorf("колесо не найдено, сначала создайте его")
-    }
-    
-    s.wheels[req.GetChatId()] = append(s.wheels[req.GetChatId()], req.GetOption())
-    return &pb.AddOptionsResponse{Message: "Опция добавлена!"}, nil
+	log.Printf("Adding option '%s' for chat_id: %d", req.GetOption(), req.GetChatId())
+	
+	if _, exists := s.wheels[req.GetChatId()]; !exists {
+		return nil, fmt.Errorf("колесо не найдено, сначала создайте его")
+	}
+	
+	s.wheels[req.GetChatId()] = append(s.wheels[req.GetChatId()], req.GetOption())
+	return &pb.AddOptionsResponse{Message: "Опция добавлена!"}, nil
 }
 
 func (s *server) SpinWheel(ctx context.Context, req *pb.SpinWheelRequest) (*pb.SpinWheelResponse, error) {
-    log.Printf("Spinning wheel for chat_id: %d", req.GetChatId())
-    
-    options, exists := s.wheels[req.GetChatId()]
-    if !exists {
-        return nil, fmt.Errorf("колесо не найдено, сначала создайте его")
-    }
-    
-    if len(options) == 0 {
-        return nil, fmt.Errorf("нет опций для выбора")
-    }
-    
-    result := options[s.rnd.Intn(len(options))]
-    return &pb.SpinWheelResponse{Result: result}, nil
+	log.Printf("Spinning wheel for chat_id: %d", req.GetChatId())
+	
+	options, exists := s.wheels[req.GetChatId()]
+	if !exists {
+		return nil, fmt.Errorf("колесо не найдено, снач��ла создайте его")
+	}
+	
+	if len(options) == 0 {
+		return nil, fmt.Errorf("нет опций для выбора")
+	}
+	
+	result := options[rand.Intn(len(options))]
+	return &pb.SpinWheelResponse{Result: result}, nil
 }
 
 func (s *server) ViewOptions(ctx context.Context, req *pb.ViewOptionsRequest) (*pb.ViewOptionsResponse, error) {
-    log.Printf("Viewing options for chat_id: %d", req.GetChatId())
-    
-    options, exists := s.wheels[req.GetChatId()]
-    if !exists {
-        return nil, fmt.Errorf("колесо не найдено, сначала создайте его")
-    }
-    
-    return &pb.ViewOptionsResponse{Options: options}, nil
+	log.Printf("Viewing options for chat_id: %d", req.GetChatId())
+	
+	options, exists := s.wheels[req.GetChatId()]
+	if !exists {
+		return nil, fmt.Errorf("колесо не найдено, сначала создайте его")
+	}
+	
+	return &pb.ViewOptionsResponse{Options: options}, nil
 }
 
 func main() {
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
-    
-    lis, err := net.Listen("tcp", port)
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    
-    s := grpc.NewServer(
-        grpc.UnaryInterceptor(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-            log.Printf("Received request: method=%s, request=%+v", info.FullMethod, req)
-            resp, err := handler(ctx, req)
-            log.Printf("Sending response: %+v, error: %v", resp, err)
-            return resp, err
-        }),
-    )
-    
-    pb.RegisterFunWheelServiceServer(s, &server{
-        wheels: make(map[int64][]string),
-        rnd:    r,
-    })
-    
-    log.Printf("Server is running on port %s", port)
-    
-    if err := s.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+	rand.Seed(time.Now().UnixNano())
+	
+	grpcServer := grpc.NewServer()
+	
+	pb.RegisterFunWheelServiceServer(grpcServer, &server{
+		wheels: make(map[int64][]string),
+	})
+
+	wrappedGrpc := grpcweb.WrapServer(grpcServer,
+		grpcweb.WithOriginFunc(func(origin string) bool {
+			return true 
+		}),
+	)
+
+	handler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Access-Control-Allow-Origin", "*")
+		resp.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		resp.Header().Set("Access-Control-Allow-Headers", "*")
+
+		if wrappedGrpc.IsGrpcWebRequest(req) || wrappedGrpc.IsAcceptableGrpcCorsRequest(req) {
+			wrappedGrpc.ServeHTTP(resp, req)
+			return
+		}
+	})
+
+	httpServer := &http.Server{
+		Addr:    ":50051",
+		Handler: handler,
+	}
+
+	log.Printf("Server is running on port :50051")
+	log.Fatal(httpServer.ListenAndServe())
 }
