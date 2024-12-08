57fd3ece3de945d4a88e988bd1d9dd2e90894592
eordiyants <erikray@mail.com>
2024-12-08 21:51:03 +0300

    sdkfjb

--- bot/bot.go
+++ bot/bot.go
@@ -8,9 +8,14 @@ import (
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		log.Fatal("BOT_TOKEN environment variable is not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
@@ -19,16 +24,32 @@ func main() {
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
+		log.Printf("Received message from %s: %s", update.Message.From.UserName, update.Message.Text)
+
 		if update.Message.Text == "/start" {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, 
-				"Создайте своё колесо фортуны: http://scrypze.ru")
-			bot.Send(msg)
+			log.Println("Received /start command")
+
+			inlineKeyboard := tgbotapi.NewInlineKeyboardMarkup(
+				[]tgbotapi.InlineKeyboardButton{
+					tgbotapi.NewInlineKeyboardButtonURL("Перейти на веб-страницу с колесом фортуны", "http://scrypze.ru"),
+				},
+			)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Нажмите на кнопку ниже, чтобы перейти к колесу фортуны!")
+			msg.ReplyMarkup = inlineKeyboard
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Printf("Failed to send message: %v", err)
+			}
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,4 @@ go 1.22
 
 require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 
-require (
-	github.com/gorilla/mux v1.8.1
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-)
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
